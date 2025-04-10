f37de323486a3296dbc8eecd23e2be5df41c25ea
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-04-10 23:19:12 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -125,7 +125,7 @@ try {
       submitButton.removeAttribute('disabled');
       submitButton.textContent = "Ro'yxatdan o'tish";
       closeModal();
-      window.location.href = `../bonus.html`
+      window.location.href = `../telegram.html`
     }
   })
 } catch (e) {
